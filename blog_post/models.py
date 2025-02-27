c016535d9f117bc1f6afa19bd77801a7541aa7df
SidenkoStas <volondss@gmail.com>
2025-02-27 19:05:59 +0300

    Добавлена модель для постлв блога

--- blog_post/models.py
+++ blog_post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    publish = models.DataTimeField(default=timezone.now)
+    created = models.DataTimeField(auto_now_add=True)
+    updated = models.DataTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-publish")
+
+    def __str__(self):
+        return self.title
