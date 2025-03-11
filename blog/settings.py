ef824a3e0d2c318960eddbbd7d7629651b69f414
SidenkoStas <volondss@gmail.com>
2025-03-11 15:55:20 +0300

    Расширена модель связбю с моделью пользователей. Добавлен индекс для столбца "-publish"

--- blog/settings.py
+++ blog/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Свои приложения
+    "blog_post.apps.BlogPostConfig",
 ]
 
 MIDDLEWARE = [
--- blog_post/models.py
+++ blog_post/models.py
@@ -1,16 +1,34 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name="blog_posts"
+        )
     body = models.TextField()
-    publish = models.DataTimeField(default=timezone.now)
-    created = models.DataTimeField(auto_now_add=True)
-    updated = models.DataTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status, default=Status.DRAFT
+        )
 
     class Meta:
         ordering = ("-publish")
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
+
+
+
