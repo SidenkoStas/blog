24e0ed15fb0a44c2b6885de5dd37e03a52a9eb3e
SidenkoStas <volondss@gmail.com>
2025-04-09 11:13:55 +0300

    Запущен Postgres в Docker. Перенесена база данных из SQLite в Postgres. Добавлено README.

--- blog/settings.py
+++ blog/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
